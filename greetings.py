782cedc4692cdde1d3d08fea23ca1fa98e3f1db7
Klara Simickova <kaya.simickova@gmail.com>
2021-09-20 16:09:54 +0200

    wrong on purpose

--- greetings.py
+++ greetings.py
@@ -3,3 +3,7 @@
 def say_hi(name):
   greeting = "Hello world! says " + name
   return greeting
+
+def say_bye(name):
+  byebye = "Goodbye " + name + " says the world"
+  return byebye
--- test_greetings.py
+++ test_greetings.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
-def test_code():
-    import greetings
+import greetings
+
+def test_greeting():
     assert greetings.say_hi("Greta") == "Hello world! says Greta"
+
+def test_byebye():
+    assert greetings.say_bye("Agnes") == "Goodbye Agnes"
